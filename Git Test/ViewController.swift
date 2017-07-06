e409f22a9aa190cc5146fa73e718325c7bb6cdfd
protrek <protrek@protrekui-MacBook-Pro.local>
2017-07-06 21:08:24 +0900

    black ground color change

--- Git Test/ViewController.swift
+++ Git Test/ViewController.swift
@@ -10,16 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        self.view.backgroundColor = UIColor.red
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+   
 
 }
 
